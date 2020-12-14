07a36df09fa99b46614486fdce307a5f0d5bb86a
bharath <kumar.bharath1994@gmail.com>
2020-12-14 13:59:34 +0530

    Added Print Statements

--- DataCollection/src/main/java/controller/Demo.java
+++ DataCollection/src/main/java/controller/Demo.java
@@ -23,6 +23,7 @@ public class Demo implements Servlet{
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("iam in service layer of DataCollection project");
+		System.out.println();
 		String name=req.getParameter("name");
 		String number =req.getParameter("number");
 		String email =req.getParameter("email");
