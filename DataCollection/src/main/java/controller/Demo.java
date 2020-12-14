ed05c3349e863f5a6dbd638b0eeb8ee7b2489a61
bharath <kumar.bharath1994@gmail.com>
2020-12-14 13:42:09 +0530

    commited DataCollection project

---
+++ DataCollection/src/main/java/controller/Demo.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Demo implements Servlet{
+
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String name=req.getParameter("name");
+		String number =req.getParameter("number");
+		String email =req.getParameter("email");
+		String password =req.getParameter("password");
+		System.out.println(name);
+		System.out.println(number);
+		System.out.println(email);
+		System.out.println(password);
+	}
+
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ DataCollection/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<html>
+<body>
+<form action="request">
+
+Name : <input type ="text" name="name"/>
+Email : <input type="email" name="email"/>
+Password : <input type="password" name="password"/>
+Phone Number : <input type="number" name="number"/>
+<input type="submit" value="sumbit"/>
+
+
+</form>
+
+
+</body>
+</html>
